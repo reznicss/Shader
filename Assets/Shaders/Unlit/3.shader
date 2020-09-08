f6f89cc3e1b5184c31eee294ad86db60c2b23a30
reznicss <reznicss@163.com>
2020-09-08 14:41:30 +0800

    通过脚本或Shader内部控制旋转、缩放

---
+++ Assets/Shaders/Unlit/3.shader
@@ -0,0 +1,42 @@
+﻿
+Shader "Unlit/3"
+{
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            
+            
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+            float4x4 rm;
+            float4x4 sm;
+            struct v2f{
+                float4 pos:POSITION;
+                fixed4 color:COLOR;
+            };
+
+            v2f vert(appdata_base v){
+                v2f o;
+                // float4 m = UnityObjectToClipPos(mul(rm,mul(sm,v.vertex)));
+                rm = float4x4(
+                cos(-_Time.y), -sin(-_Time.y), 0, 0,
+                sin(-_Time.y), cos(-_Time.y), 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
+                float4 m = UnityObjectToClipPos(mul(rm,v.vertex));
+                // o.pos = mul(rm,m);
+                o.pos = m;
+                return o;
+            }
+
+            fixed4 frag(v2f IN):COLOR{
+                return fixed4(1,1,1,1);
+            }
+            ENDCG
+            
+        }
+    }
+}
---
+++ Assets/Shaders/Unlit/Demo3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Demo3 : MonoBehaviour
+{
+    Matrix4x4 _rm = Matrix4x4.identity;
+    Matrix4x4 _sm = Matrix4x4.identity;
+    Material material;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        //绕X
+        // rm.m11 = Mathf.Cos(Time.realtimeSinceStartup);
+        // rm.m12 = -Mathf.Sin(Time.realtimeSinceStartup);
+        // rm.m21 = Mathf.Sin(Time.realtimeSinceStartup);
+        // rm.m22 = Mathf.Cos(Time.realtimeSinceStartup);
+
+        //绕Y
+        // rm.m00 = Mathf.Cos(Time.realtimeSinceStartup);
+        // rm.m02 = Mathf.Sin(Time.realtimeSinceStartup);
+        // rm.m20 = -Mathf.Sin(Time.realtimeSinceStartup);
+        // rm.m22 = Mathf.Cos(Time.realtimeSinceStartup);
+
+        //绕Z
+        _rm.m00 = Mathf.Cos(Time.realtimeSinceStartup);
+        _rm.m01 = -Mathf.Sin(Time.realtimeSinceStartup);
+        _rm.m10 = Mathf.Sin(Time.realtimeSinceStartup);
+        _rm.m11 = Mathf.Cos(Time.realtimeSinceStartup);
+
+        _sm.m00 = Mathf.Sin(Time.realtimeSinceStartup);
+        _sm.m11 = Mathf.Cos(Time.realtimeSinceStartup);
+        _sm.m22 = Mathf.Sin(Time.realtimeSinceStartup);
+
+        material.SetMatrix("rm", _rm);
+        material.SetMatrix("sm",_sm);
+
+    }
+}
